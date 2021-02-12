02168b17c29a8d4e42852da3462e580aa9b6c511
as30098 <alexspencer421@gmail.com>
2021-02-12 14:30:44 -0500

    auth stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::API
-    # def authenticate
+    def authenticate
         # TODO: check the user's token
         # if their token is valid, get their details from the database
         # return that user
         # if not, return some error message
-        # @current_user = User.first
-    # end
+        @current_user = User.first
+    end
 end

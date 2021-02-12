fc9479e01c83be284403952ad18b608b34733317
as30098 <alexspencer421@gmail.com>
2021-02-12 11:52:49 -0500

    fake auth updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+  #def login
+    #render json: User.first
+  #end
+  # we just need to testt localhost:3000/login in Postman to verify that it returns our first user obj
 end
